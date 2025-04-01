26eea2d699ff7c1f2351d234e7b85fe52476bb23
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-04-01 23:09:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+//your code he
 
+
+let ol = document.querySelector("#infi-list");
+
+function addItems(){
+	Array.from({length:18}).forEach((_,i)=>{
+		ol.innerHTML+=`<li>Item ${i+1} </li>`
+	})
+}
+
+window.addEventListener("DOMContentLoaded",addItems);
+ol.addEventListener("scroll",()=>{
+	// alert("hello")
+	// if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10){
+		addItems();
+	// }
+})
